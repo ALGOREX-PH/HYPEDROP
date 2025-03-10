dfcc7f0d2c1b71f6290e880288f68c7df155a0b0
unknown <debbiemeer30@gmail.com>
2025-03-11 02:26:57 +0800

    updated nav component

--- components/nav.tsx
+++ components/nav.tsx
@@ -57,16 +57,18 @@ export function Nav() {
               </div>
             </div>
           </div>
-          <Link href="/auth">
-          <Button className="bg-violet-600 hover:bg-violet-700 hidden sm:inline-flex">
-            Sign Up
-          </Button>
-          </Link>
-          <Link href="/cart">
-            <Button variant="ghost" size="icon" className="ml-2">
-              <ShoppingCart className="w-5 h-5" />
-            </Button>
-          </Link>
+          <div className="flex items-center gap-2">
+            <Link href="/auth">
+              <Button className="bg-violet-600 hover:bg-violet-700 hidden sm:inline-flex">
+                Sign Up
+              </Button>
+            </Link>
+            <Link href="/cart">
+              <Button variant="ghost" size="icon">
+                <ShoppingCart className="w-5 h-5" />
+              </Button>
+            </Link>
+          </div>
         </div>
       </div>
     </nav>
